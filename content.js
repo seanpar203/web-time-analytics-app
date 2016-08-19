7eadcb4a8fa1ccdf038a4cda1fe2dbce7aad4a50
Sean Parsons <sean@labelinteractive.com>
2016-08-18 21:01:05 -0400

    Wrapped content.js in annon self invoking function for encapsulation.

--- content.js
+++ content.js
@@ -2,13 +2,14 @@
  * Created by sean on 15/08/2016.
  */
 
-// Basic Variables
-let host = window.location.host;
-let sendMessage = chrome.runtime.sendMessage;
+(() => {
+	let host = window.location.host;
+	let sendMessage = chrome.runtime.sendMessage;
 
-sendMessage({
-    message: 'newTab',
-    tabUrl: host
-}, (response) => {
-    console.log(response.farewell);
-});
+	sendMessage({
+		message: 'newTab',
+		tab: host
+	}, (response) => {
+		console.log(response.message);
+	});
+})();
