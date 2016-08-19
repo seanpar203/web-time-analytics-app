f6bd46752b57ba620d044ee5a18a952926f1d3c3
Sean Parsons <sean@labelinteractive.com>
2016-08-18 21:02:05 -0400

    Reformmated code in popup.js to improve readability.

--- popup.js
+++ popup.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    "use strict";
+	"use strict";
 
-    // Set Basic Variable Defaults.
-    let btn = document.getElementById('btn');
-    let bkp = chrome.extension.getBackgroundPage();
-    let tabQuery = chrome.tabs.query;
+	// Set Basic Variable Defaults.
+	let btn = document.getElementById('btn');
+	let bkp = chrome.extension.getBackgroundPage();
+	let tabQuery = chrome.tabs.query;
 
-    btn.addEventListener('click', () => {
-        tabQuery({active: true}, (tabs) => {
-            let [tab] = tabs;
-            bkp.console.log(tab)
-        });
-    });
+	btn.addEventListener('click', () => {
+		tabQuery({
+				active: true,
+				currentWindow: true
+			},
+			(tabs) => {
+				let [tab] = tabs;
+				bkp.console.log(tab)
+			});
+	});
 });
