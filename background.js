a48174e638f4e994cf9a8fa2561bf20ac3ce8dfe
Sean Parsons <seanpatrick2013@gmail.com>
2016-08-23 14:41:04 -0400

    Cleaned up class interfaces.

--- background.js
+++ background.js
@@ -4,126 +4,140 @@
 (() => {
 
 
-    /** Class Representing User State */
-    class User {
-        /**
-         * Creates user state.
-         * @constructor
-         */
-        constructor() {
-            this.uid = '';
-        }
+	/** Class Representing User State */
+	class User {
+		/**
+		 * Creates user state.
+		 * @constructor
+		 */
+		constructor() {
+			this.uid = '';
+		}
 
-        /**
-         * Sets Users attributes.
-         * @param {string} token - Unique string value.
-         */
-        set token(token) {
-            this.uid = token;
-        }
-    }
+		/**
+		 * Sets Users attributes.
+		 * @param {string} token - Unique string value.
+		 */
+		set token(token) {
+			this.uid = token;
+		}
 
-    /** Class Representing Counter State */
-    class Counter {
-        /**
-         * Creates counter state.
-         * @constructor
-         */
-        constructor() {
-            this.seconds = 0;
-        }
+		/**
+		 * Returns object with uid value.
+		 * @returns {{uid: (string|*|string)}} The value of uid.
+		 */
+		get state() {
+			return {
+				uid: this.uid
+			}
+		}
+	}
 
-        /** startCounter interval callback. */
-        increment() {
-            this.seconds++;
-        }
+	/** Class Representing Counter State */
+	class Counter {
+		/**
+		 * Creates counter state.
+		 * @constructor
+		 */
+		constructor() {
+			this.seconds = 0;
+		}
 
-        /** Start counter interval. */
-        startCounter() {
-            const interval = setInterval(this.increment, 1000);
-        }
+		/**
+		 * Returns object with seconds value.
+		 * @returns {{seconds: number}} The value of seconds.
+		 */
+		get state() {
+			return {
+				seconds: this.seconds
+			}
+		}
 
-        /** Stops counter interval. */
-        stopCounter() {
-            clearInterval(interval)
-        }
-    }
+		/** startCounter interval callback. */
+		increment() {
+			this.seconds++;
+		}
 
-    /** Class Representing Tab State */
-    class Tab {
+		/** Start counter interval. */
+		start() {
+			const interval = setInterval(this.increment, 1000);
+		}
 
-        /**
-         * Creates tab state.
-         * @constructor
-         */
-        constructor() {
-            this.tabHost = '';
-            this.tabSet = false;
-        }
+		/** Stops counter interval. */
+		stop() {
+			clearInterval(interval)
+		}
+	}
 
-        /**
-         * Sets new tab state.
-         * @param {string} host - The new host name of active tab.
-         */
-        set host(host) {
-            this.tabHost = host;
-            this.tabSet = true;
-        }
+	/** Class Representing Tab State */
+	class Tab {
 
-        /**
-         * Gets the current tab state.
-         * @returns {{host: (string|*)}} The tabHost value.
-         */
-        get tab() {
-            return {
-                host: this.tabHost,
-            }
-        }
-    }
+		/**
+		 * Creates tab state.
+		 * @constructor
+		 */
+		constructor() {
+			this.tabHost = '';
+			this.tabSet = false;
+		}
+
+		/**
+		 * Sets new tab state.
+		 * @param {string} host - The new host name of active tab.
+		 */
+		set host(host) {
+			this.tabHost = host;
+			this.tabSet = true;
+		}
+
+		/**
+		 * Gets the current tab state.
+		 * @returns {{host: (string|*)}} The tabHost value.
+		 */
+		get state() {
+			return {
+				host: this.tabHost,
+			}
+		}
+	}
 
 
-    /** Master Class Representing Main Interface. */
-    class BackgroundManager {
+	/** Master Class Representing Main Interface. */
+	class BackgroundManager {
 
-        /**
-         * Creates background state.
-         * @constructor
-         */
-        constructor() {
-            let _tab = new Tab();
-            let _user = new User();
-            let _counter = new Counter();
+		/**
+		 * Creates background state.
+		 * @constructor
+		 */
+		constructor() {
+			let _tab = new Tab();
+			let _user = new User();
+			let _counter = new Counter();
 
-            /**
-             * onActivated callback.
-             * @param {object} tab - object containing tab id and window id.
-             */
-            this.activateHandler = (tab) => {
-                // Message to send to content.js
-                let msg = {
-                    message: 'host'
-                };
+			/**
+			 * onActivated callback.
+			 * @param {object} tab - object containing tab id and window id.
+			 */
+			this.activateHandler = tab => {
+				let msg = {
+					message: 'host'
+				};
 
-                // sendMessage(tab.tabId, msg, res => {
-                //     if (!this.tabSet) {
-                //         this.host = res.host;
-                //     } else {
-                //         this.saveTab(this.tab);
-                //         this.host = res.host;
-                //     }
-                // });
-            }
-        }
-    }
+				sendMessage(tab.tabId, msg, res => {
+					console.log(res);
+				});
+			}
+		}
+	}
 
-    // Create new Instances of Classes.
-    const BGM = new BackgroundManager();
+	// Create new Instances of Classes.
+	const BGM = new BackgroundManager();
 
-    // Chrome API Reassignment.
-    const tabQuery = chrome.tabs.query;
-    const sendMessage = chrome.tabs.sendMessage;
+	// Chrome API Reassignment.
+	const tabQuery = chrome.tabs.query;
+	const sendMessage = chrome.tabs.sendMessage;
 
-    // Chrome Listeners & Handlers.
-    chrome.tabs.onActivated.addListener(BGM.activateHandler);
+	// Chrome Listeners & Handlers.
+	chrome.tabs.onActivated.addListener(BGM.activateHandler);
 
 })();
