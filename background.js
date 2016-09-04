940e78e2244981d309f9f96696cdcc0ea6bbaab4
Sean Parsons <seanpatrick2013@gmail.com>
2016-09-04 15:30:20 -0400

    Refactored classes to expose less methods.

--- background.js
+++ background.js
@@ -2,169 +2,135 @@
 
 (() => {
 
-    // Chrome API Reassignment.
-    const tabQuery = chrome.tabs.query;
-    const setStorage = chrome.storage.sync.set;
-    const getStorage = chrome.storage.sync.get;
-    const sendMessage = chrome.tabs.sendMessage;
+	// Chrome API Reassignment.
+	const tabQuery = chrome.tabs.query;
+	const setStorage = chrome.storage.sync.set;
+	const getStorage = chrome.storage.sync.get;
+	const sendMessage = chrome.tabs.sendMessage;
 
-    // Helper functions that don't add to classes interface.
-    /**
-     * Returns whether a object has keys or not.
-     * @param {object} obj
-     * @returns {boolean}
-     */
-    function isEmptyObject(obj) {
-        if (obj === undefined) {
-            return true;
-        }
-        return Object.keys(obj).length === 0;
-    }
+	// Helper functions that don't add to classes interface.
+	/**
+	 * Returns whether a object has keys or not.
+	 * @param {object} obj
+	 * @returns {boolean}
+	 */
+	function isEmptyObject(obj) {
+		if (obj === undefined) {
+			return true;
+		}
+		return Object.keys(obj).length === 0;
+	}
 
-    /**
-     * Generates Token, saves it and returns it.
-     * @returns {string} value of gen'd token.
-     */
-    function genToken() {
-        let token = uuid.v4();
-        setStorage({
-            'WTA_TOKEN': token
-        });
-        return token;
-    }
+	/**
+	 * Generates Token, saves it and returns it.
+	 * @returns {string} value of gen'd token.
+	 */
+	function genToken() {
+		let token = uuid.v4();
+		setStorage({'WTA_TOKEN': token});
+		return token;
+	}
 
 
-    /** Class Representing User State */
-    class User {
-        /**
-         * Creates user state.
-         * @constructor
-         */
-        constructor() {
-            this.uid = '';
-            this.getOrCreate();
-        }
-
-        /**
-         * Returns object with uid value.
-         * @returns {{uid: (string|*|string)}} The value of uid.
-         */
-        get state() {
-            return {
-                uid: this.uid
-            }
-        }
-
-        /**
-         * Gets or creates new uid token.
-         */
-        getOrCreate() {
-            getStorage('WTA_TOKEN', obj => {
-                this.uid = isEmptyObject(obj) ? genToken() : obj.WTA_TOKEN;
-            });
-        }
-    }
-
-    /** Class Representing Counter State */
-    class Counter {
-        /**
-         * Creates counter state.
-         * @constructor
-         */
-        constructor() {
-            this.seconds = 0;
-        }
-
-        /**
-         * Returns object with seconds value.
-         * @returns {{seconds: number}} The value of seconds.
-         */
-        get state() {
-            return {
-                seconds: this.seconds
-            }
-        }
-
-        /** startCounter interval callback. */
-        increment() {
-            this.seconds++;
-        }
-
-        /** Start counter interval. */
-        start() {
-            const interval = setInterval(this.increment, 1000);
-        }
-
-        /** Stops counter interval. */
-        stop() {
-            clearInterval(interval)
-        }
-    }
-
-    /** Class Representing Tab State */
-    class Tab {
-
-        /**
-         * Creates tab state.
-         * @constructor
-         */
-        constructor() {
-            this.tabHost = '';
-            this.tabSet = false;
-        }
-
-        /**
-         * Sets new tab state.
-         * @param {string} host - The new host name of active tab.
-         */
-        set host(host) {
-            this.tabHost = host;
-            this.tabSet = true;
-        }
-
-        /**
-         * Gets the current tab state.
-         * @returns {{host: (string|*)}} The tabHost value.
-         */
-        get state() {
-            return {
-                host: this.tabHost,
-            }
-        }
-    }
+	/** Class Representing Token State */
+	class Token {
+		/**
+		 * Creates token state.
+		 * @constructor
+		 */
+		constructor() {
+			// Attributes
+			let _newUser = false;
+			let _getOrCreate = () => getStorage('WTA_TOKEN', _validateToken);
+			const _validateToken = (obj) => {
+				if (isEmptyObject(obj)) {
+					_newUser = true;
+					return genToken()
+				}
+				return obj.WTA_TOKEN;
+			};
+			const _token = _getOrCreate();
 
 
-    /** Master Class Representing Main Interface. */
-    class BackgroundManager {
+			// Exposed Methods.
+			/** Returns object with token value. */
+			this.state = () => ({token: _token});
+		}
+	}
 
-        /**
-         * Creates background state.
-         * @constructor
-         */
-        constructor() {
-            let _tab = new Tab();
-            let _user = new User();
-            let _counter = new Counter();
+	/** Class Representing Counter State */
+	class Counter {
+		/**
+		 * Creates counter state.
+		 * @constructor
+		 */
+		constructor() {
+			// Attributes
+			let _seconds = 0;
+			const _increment = () => _seconds++;
 
-            /**
-             * onActivated callback.
-             * @param {object} tab - object containing tab id and window id.
-             */
-            this.onActivated = tab => {
-                let msg = {
-                    message: 'host'
-                };
+			// Exposed Methods.
+			/** Returns object with minutes value. */
+			this.state = () => ({seconds: _seconds / 60});
 
-                sendMessage(tab.tabId, msg, res => {
-                    console.log(res);
-                });
-            };
-        }
-    }
+			/** Start counter interval. */
+			this.start = () => {const interval = setInterval(_increment, 1000);};
 
-    // Create new Instances of Manager.
-    const BGM = new BackgroundManager();
+			/** Stops counter interval. */
+			this.stop = () => clearInterval(interval);
+		}
+	}
 
-    // Chrome Listeners & Handlers.
-    chrome.tabs.onActivated.addListener(BGM.onActivated);
+	/** Class Representing host name State */
+	class HostName {
+
+		/**
+		 * Creates host name state.
+		 * @constructor
+		 */
+		constructor() {
+			let _hostName = '';
+
+			/** Sets new host name state. */
+			this.setHost = (host) => _hostName = host;
+
+			/** Gets the current host name state. */
+			this.state = () => ({host: _hostName,})
+		}
+	}
+
+
+	/** Master Class Representing Main Interface. */
+	class BackgroundManager {
+
+		/**
+		 * Creates background state.
+		 * @constructor
+		 */
+		constructor() {
+			let _host = new HostName();
+			let _token = new Token();
+			let _counter = new Counter();
+
+			/**
+			 * onActivated callback.
+			 * @param {object} tab - object containing tab id and window id.
+			 */
+			this.onActivated = tab => {
+				let msg = {
+					message: 'host'
+				};
+
+				sendMessage(tab.tabId, msg, res => {
+					console.log(res);
+				});
+			};
+		}
+	}
+
+	// Create new Instances of Manager.
+	const BGM = new BackgroundManager();
+
+	// Chrome Listeners & Handlers.
+	chrome.tabs.onActivated.addListener(BGM.onActivated);
 })();
