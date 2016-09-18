14142067eae3a4ec7e3cf3d256a0b2831f651602
Sean Parsons <seanpatrick2013@gmail.com>
2016-09-17 21:16:59 -0400

    Updated styles & js, fixed bug where wouldn't render line because color undefined.

--- assets/js/time-spent.js
+++ assets/js/time-spent.js
@@ -171,42 +171,59 @@ $(() => {
 		data = data.map(d => Object.assign({}, d));
 
 		// Set the dimensions of the canvas / graph
-		var margin = {top: 30, right: 20, bottom: 70, left: 50},
-		    width  = 800 - margin.left - margin.right,
-		    height = 400 - margin.top - margin.bottom;
+		var margin = {
+				top: 30,
+				right: 20,
+				bottom: 70,
+				left: 50
+			},
+			width = 800 - margin.left - margin.right,
+			height = 400 - margin.top - margin.bottom;
 
 		// Parse the date / time
 		var parseDate = d3.time.format("%Y-%m-%d").parse;
 
 		// Set the ranges
-		var x = d3.time.scale().range([0, width]);
-		var y = d3.scale.linear().range([height, 0]);
+		var x = d3.time.scale()
+		          .range([
+			          0,
+			          width
+		          ]);
+		var y = d3.scale.linear()
+		          .range([
+			          height,
+			          0
+		          ]);
 
 		// Define the axes
-		var xAxis = d3.svg.axis().scale(x)
-			.orient("bottom").ticks(5);
+		var xAxis = d3.svg.axis()
+		              .scale(x)
+		              .orient("bottom")
+		              .ticks(5);
 
-		var yAxis = d3.svg.axis().scale(y)
-			.orient("left").ticks(5);
+		var yAxis = d3.svg.axis()
+		              .scale(y)
+		              .orient("left")
+		              .ticks(5);
 
 		// Define the line
 		var priceline = d3.svg.line()
-			.x(function (d) {
-				return x(d.date);
-			})
-			.y(function (d) {
-				return y(d.time);
-			});
+		                  .x(function (d) {
+			                  return x(d.date);
+		                  })
+		                  .y(function (d) {
+			                  return y(d.time);
+		                  });
 
 		// Adds the svg canvas
 		var svg = d3.select("#trends")
-			.append("svg")
-			.attr('class', 'trends')
-			.attr("width", width + margin.left + margin.right)
-			.attr("height", height + margin.top + margin.bottom)
-			.append("g")
-			.attr("transform",
-				"translate(" + margin.left + "," + margin.top + ")");
+		            .append("svg")
+		            .attr('class', 'trends')
+		            .attr("width", width + margin.left + margin.right)
+		            .attr("height", height + margin.top + margin.bottom)
+		            .append("g")
+		            .attr("transform",
+			            "translate(" + margin.left + "," + margin.top + ")");
 
 		data.forEach(function (d) {
 			d.date = parseDate(d.date);
@@ -217,58 +234,48 @@ $(() => {
 		x.domain(d3.extent(data, function (d) {
 			return d.date;
 		}));
-		y.domain([0, d3.max(data, function (d) {
-			return d.time;
-		})]);
+		y.domain([
+			0,
+			d3.max(data, function (d) {
+				return d.time;
+			})
+		]);
 
 		// Nest the entries by host
 		var dataNest = d3.nest()
-			.key(d =>  d.host)
-			.entries(data);
+		                 .key(d => d.host)
+		                 .entries(data);
 
-		var legendSpace = width / dataNest.length; // spacing for the legend
 
+		$('#trends-hierarchy').empty();
 		// Loop through each host / key
 		dataNest.forEach(function (d, i) {
 
 			svg.append("path")
-				.attr("class", "line")
-				.style("stroke", d.values[i].color)
-				.attr("id", 'tag' + d.key.replace(/\s+/g, '')) // assign ID
-				.attr("d", priceline(d.values));
+			   .attr("class", "line")
+			   .style("stroke", d.values[0].color)
+			   .attr("id", 'tag' + d.key.replace(/\s+/g, '')) // assign ID
+			   .attr("d", priceline(d.values));
 
-			// Add the Legend
-			svg.append("text")
-				.attr("x", (legendSpace / 2) + i * legendSpace)  // space legend
-				.attr("y", height + (margin.bottom / 2) + 5)
-				.attr("class", "legend")    // style the legend
-				.style("fill", d.values[i].color)
-				.on("click", function () {
-					// Determine if current line is visible
-					var active     = d.active ? false : true,
-					    newOpacity = active ? 0 : 1;
-					// Hide or show the elements based on the ID
-					d3.select("#tag" + d.key.replace(/\s+/g, ''))
-						.transition().duration(100)
-						.style("opacity", newOpacity);
-					// Update whether or not the elements are active
-					d.active = active;
-				})
-				.text(d.key);
+			$('#trends-hierarchy')
+				.append(
+					'<li id=' + d.key + '>' +
+					'<span style="background-color:'+d.values[0].color+ '">' + '</span>' + d.key +
+					'</li>'
+				);
 
-		});
+			// Add the X Axis
+			svg.append("g")
+			   .attr("class", "x axis")
+			   .attr("transform", "translate(0," + height + ")")
+			   .call(xAxis);
 
-		// Add the X Axis
-		svg.append("g")
-			.attr("class", "x axis")
-			.attr("transform", "translate(0," + height + ")")
-			.call(xAxis);
+			// Add the Y Axis
+			svg.append("g")
+			   .attr("class", "y axis")
+			   .call(yAxis);
 
-		// Add the Y Axis
-		svg.append("g")
-			.attr("class", "y axis")
-			.call(yAxis);
-
+		})
 	}
 
 		if (token != null) {
